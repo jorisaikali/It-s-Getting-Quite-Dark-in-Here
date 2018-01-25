98cfabd57c511f22e71749613a477d6c72d5f554
jorisaikali <jori.saikali@hotmail.com>
2018-01-24 23:07:56 -0500

    Added player movement functionality (feature)

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ displayHeight = 600
 screen = pygame.display.set_mode((displayWidth, displayHeight))
 """ --------------------------------------------------- """
 
-""" -------------- Setting up fps -------------- """
+""" -------------- Setting up fps and clock -------------- """
 FPS = 60
 REFRESH = pygame.USEREVENT + 1
 pygame.time.set_timer(REFRESH, 1000 // FPS)
-""" -------------------------------------------- """
+
+clock = pygame.time.Clock()
+""" ------------------------------------------------------ """
 
 """ -------------- Setting up the background -------------- """
 menuBackground = (0,0,0)
@@ -22,10 +24,11 @@ gameBackground = pygame.image.load(os.path.join("images", "gameBackground.jpg"))
 """ --------------------------------------------- Player Class --------------------------------------------- """
 class player:
 	def __init__(self):
-		self.x = displayWidth / 2
-		self.y = displayHeight / 2
 		self.anim = glob.glob(os.path.join("images", "player*.png")) # 0 = down, 1 = left, 2 = right, 3 = up
 		self.img = pygame.image.load(self.anim[3])
+		self.imgRect = self.img.get_rect()
+		self.x = displayWidth // 2 - self.imgRect.width // 2
+		self.y = displayHeight // 2 - self.imgRect.height // 2
 		self.update(0, 0, "up")
 
 	def update(self, deltaPosX, deltaPosY, direction):
@@ -48,24 +51,43 @@ class player:
 				self.img = pygame.image.load(self.anim[2])
 			# ----------------------------------------------------- #
 
+		self.imgRect = self.img.get_rect()
 		screen.blit(self.img, (self.x, self.y))
 """ -------------------------------------------------------------------------------------------------------- """
 
-""" ---- Global variables ---- """
-player = player()
-deltaPosX = 0
-deltaPosY = 0
-direction = "up"
-""" -------------------------- """
-
 """ ---------------- Game Loop ---------------- """
 def run():
+	deltaPosX = 0
+	deltaPosY = 0
+	direction = "up"
+
 	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-			elif event.type == REFRESH:
-				draw()
+		#for event in pygame.event.get():
+		event = pygame.event.wait()
+
+		if event.type == pygame.QUIT:
+			sys.exit()
+		elif event.type == REFRESH:
+			draw()
+
+		if event.type == pygame.KEYDOWN and event.key == pygame.K_w:
+			deltaPosX = 0
+			deltaPosY = -5
+			direction = "up"
+		elif event.type == pygame.KEYDOWN and event.key == pygame.K_s:
+			deltaPosX = 0
+			deltaPosY = 5
+			direction = "down"
+		elif event.type == pygame.KEYDOWN and event.key == pygame.K_a:
+			deltaPosX = -5
+			deltaPosY = 0
+			direction = "left"
+		elif event.type == pygame.KEYDOWN and event.key == pygame.K_d:
+			deltaPosX = 5
+			deltaPosY = 0
+			direction = "right"
+
+		limitBorders()
 
 		player.update(deltaPosX, deltaPosY, direction)
 		pygame.display.update()
@@ -76,6 +98,30 @@ def draw():
 	screen.blit(gameBackground, (0,0))
 """ ---------------------------------------------- """
 
+""" ------------ Limiting border function ------------"""
+def limitBorders():
+	# ---------- Limiting upper border ---------- #
+	if player.y <= 54:
+		player.y = 54
+	# ------------------------------------------- #
+
+	# ---------- Limiting lower border ---------- #
+	if player.y >= 544 - player.imgRect.height:
+		player.y = 544 - player.imgRect.height
+	# ------------------------------------------- #
+
+	# ----------- Limiting left border ---------- #
+	if player.x <= 53:
+		player.x = 53
+	# ------------------------------------------- #
+
+	# ---------- Limiting right border ---------- #
+	if player.x >= 744 - player.imgRect.width:
+		player.x = 744 - player.imgRect.width
+	# ------------------------------------------- #
+""" ------------------------------------------------ """
+
+player = player()
 run()
 pygame.quit()
 sys.exit()
